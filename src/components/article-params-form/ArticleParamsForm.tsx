89574e6066702a0d8e9c282e1d33d3b344ad6506
Diana Novikova <bogdanova.4.diana@yandex.ru>
2024-11-06 22:03:48 +0300

    Добавила выбор шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,22 +2,61 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
+import { ArticleStateType, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { Select } from '../select';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleSettings: ArticleStateType;
+	setCurrentArticle: (article: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleSettings,
+	setCurrentArticle,
+}: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [selectedArticleSettings, setSelectedArticleSettings] = useState(articleSettings);
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setCurrentArticle({
+			...selectedArticleSettings,
+		});
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(!isOpen),
+		onChange: setIsOpen,
+	});
+
 	return (
-		<>
+		<div ref={rootRef}>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
-			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form}>
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
+					<Select
+						options={fontFamilyOptions}
+						selected={selectedArticleSettings.fontFamilyOption}
+						onChange={(data) =>
+							setSelectedArticleSettings({
+								...selectedArticleSettings,
+								fontFamilyOption: data,
+							})
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState as defaultArticleSettingsState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,24 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [currentArticleSettingsState, setCurrentArticleSettingsState] =
+		useState(defaultArticleSettingsState);
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentArticleSettingsState.fontFamilyOption.value,
+					'--font-size': currentArticleSettingsState.fontSizeOption.value,
+					'--font-color': currentArticleSettingsState.fontColor.value,
+					'--container-width': currentArticleSettingsState.contentWidth.value,
+					'--bg-color': currentArticleSettingsState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				articleSettings={currentArticleSettingsState}
+				setCurrentArticle={setCurrentArticleSettingsState}
+			/>
 			<Article />
 		</div>
 	);
